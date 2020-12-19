16e1ead6ff45781875d07de2a3fd090f5ce09457
Dwigoric <dwigoric@gmail.com>
2020-12-19 09:21:55 +0800

    misc: update JSDoc

--- src/lib/SpotifyParser.ts
+++ src/lib/SpotifyParser.ts
@@ -47,9 +47,28 @@ export interface SpotifyTrack {
 	duration_ms: number;
 }
 
+
+/**
+ * @typedef FetchOptions
+ * @name FetchOptions
+ * @type {object}
+ * @property {boolean} prioritizeSameDuration - Whether to prioritize Lavalink tracks with the same duration as the Spotify track.
+ */
 export interface FetchOptions {
 	prioritizeSameDuration: boolean;
+	/**
+	 * @param lavalinkTrack The Lavalink track for each result. Changes each iteration.
+	 * @param spotifyTrack The Spotify track equivalent to the Lavalink track. Changes each iteration.
+	 * @returns {boolean} A boolean result of the compare function, whether the Lavalink track stays or is discarded.
+	 */
 	customFilter(lavalinkTrack: LavalinkTrack, spotifyTrack: SpotifyTrack): boolean;
+	/**
+	 * 
+	 * @param comparableTrack The Lavalink track being compared to the other Lavalink tracks. Changes each iteration.
+	 * @param compareToTrack The other Lavalink track that the comparable Lavalink track is being compared to. Note that the function is run against each of the other Lavalink tracks.
+	 * @param spotifyTrack The Spotify track equivalent to the comparable Lavalink track that you can use to compare the Lavalink tracks with each other. Changes each iteration.
+	 * @returns {number} A number that defines the position of the comparableTrack in the results. The track moves to the first if a negative number is returned, 0 if it would stay at the same position with respect to the other tracks, and a positive number if it would move to the end.
+	 */
 	customSort(comparableTrack: LavalinkTrack, compareToTrack: LavalinkTrack, spotifyTrack: SpotifyTrack): number;
 }
 
@@ -62,9 +81,9 @@ export class SpotifyParser {
 
 	/**
 	 * A class to convert Spotify URLs into Lavalink track objects.
-	 * @param Node A lavalink node to expose the lavalink API
-	 * @param clientID Your Spotify's client ID.
-	 * @param clientSecret Your Spotify's client secret.
+	 * @param {Node} Node A lavalink node to expose the lavalink API
+	 * @param {string} clientID Your Spotify's client ID.
+	 * @param {string} clientSecret Your Spotify's client secret.
 	 */
 	constructor(LavalinkNode: Node, clientID: string, clientSecret: string) {
 		this.nodes = LavalinkNode;
@@ -82,8 +101,10 @@ export class SpotifyParser {
 
 	/**
 	 * Fetch the tracks from the album and return the SpotifyTrack or LavalinkTrack objects.
-	 * @param id The album ID.
-	 * @param convert Whether to return results as LavalinkTrack objects instead of SpotifyTrack objects.
+	 * @param {string} id The album ID.
+	 * @param {boolean} convert Whether to return results as LavalinkTrack objects instead of SpotifyTrack objects.
+	 * @param {FetchOptions} fetchOptions An object containing the options for fetching Lavalink tracks with the Spotify tracks.
+	 * @returns {Promise<LavalinkTrack[]|SpotifyTrack[]>} The promisified array of the tracks in the album, in the form of a Lavalink Track object (if converted) or a Spotify Track object.
 	 */
 	public async getAlbumTracks(id: string, convert = false, fetchOptions: FetchOptions): Promise<LavalinkTrack[]|SpotifyTrack[]> {
 		if (!id) throw new ReferenceError("The album ID was not provided");
@@ -96,9 +117,11 @@ export class SpotifyParser {
 	}
 
 	/**
-	 * Fetch the tracks from the playlist and return the SpotifyTrack or LavalinkTrack objects.
-	 * @param id The playlist ID.
-	 * @param convert Whether to return results as LavalinkTrack objects instead of SpotifyTrack objects.
+	 * Fetch the tracks from the album and return the SpotifyTrack or LavalinkTrack objects.
+	 * @param {string} id The album ID.
+	 * @param {boolean} convert - Whether to return results as LavalinkTrack objects instead of SpotifyTrack objects.
+	 * @param {FetchOptions} fetchOptions An object containing the options for fetching Lavalink tracks with the Spotify tracks.
+	 * @returns {Promise<LavalinkTrack[]|SpotifyTrack[]>} The promisified array of the tracks in the playlist, in the form of a Lavalink Track object (if converted) or a Spotify Track object.
 	 */
 	public async getPlaylistTracks(id: string, convert = false, fetchOptions: FetchOptions): Promise<LavalinkTrack[]|SpotifyTrack[]> {
 		if (!id) throw new ReferenceError("The playlist ID was not provided");
@@ -111,9 +134,11 @@ export class SpotifyParser {
 	}
 
 	/**
-	 * Fetch the track and return its SpotifyTrack or LavalinkTrack object.
-	 * @param id The song ID.
-	 * @param convert Whether to return results as LavalinkTracks objects instead of SpotifyTrack objects.
+	 * Fetch the tracks from the album and return the SpotifyTrack or LavalinkTrack objects.
+	 * @param {string} id The album ID.
+	 * @param {boolean} convert Whether to return results as LavalinkTrack objects instead of SpotifyTrack objects.
+	 * @param {FetchOptions} fetchOptions An object containing the options for fetching Lavalink tracks with the Spotify tracks.
+	 * @returns {Promise<LavalinkTrack|SpotifyTrack>} The promisified Lavalink Track object (if converted) or the Spotify Track object.
 	 */
 	public async getTrack(id: string, convert = false, fetchOptions: FetchOptions): Promise<LavalinkTrack|SpotifyTrack> {
 		if (!id) throw new ReferenceError("The track ID was not provided");
@@ -127,7 +152,9 @@ export class SpotifyParser {
 
 	/**
 	 * Return a LavalinkTrack object from the SpotifyTrack object.
-	 * @param track The SpotifyTrack object to be searched and compared against the Lavalink API
+	 * @param {SpotifyTrack} track The SpotifyTrack object to be searched and compared against the Lavalink API
+	 * @param {FetchOptions} fetchOptions An object containing the options for fetching Lavalink tracks with the Spotify tracks.
+	 * @returns {Promise<LavalinkTrack|null>} The promisified Lavalink Track object, or null if no match found.
 	 */
 	public async fetchTrack(track: SpotifyTrack, fetchOptions = { prioritizeSameDuration: false, customFilter: () => true, customSort: () => 0 } as FetchOptions): Promise<LavalinkTrack|null> {
 		if (!track) throw new ReferenceError("The Spotify track object was not provided");
